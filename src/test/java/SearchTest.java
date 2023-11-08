f64e5e2e44ea59e047af7006eaa4018e508c8ee4
GaciuRadu <gaciuradu@gmail.com>
2023-11-08 14:45:01 +0200

    New tests in SearchTest

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -20,19 +20,61 @@ public class SearchTest extends Hooks {
         searchPage.searchPage("turcu");
         searchPage.pushSearchButton();
         searchPage.searchPageClear();
-
         checkoutPage.assertTextWhenReady(searchPage.productShaorma, "SHAORMA LA TURCU");
+        System.out.println("Yes, I see the text1!");
+
         searchPage.searchPage("shaorma");
         searchPage.pushSearchButton();
         searchPage.searchPageClear();
-
         checkoutPage.assertTextWhenReady(searchPage.productShaorma, "SHAORMA LA TURCU");
+        System.out.println("Yes, I see the text2!");
+
 //        searchPage.searchPage("shaorma la turcu");
 //        searchPage.pushSearchButton();
 //        System.out.println("Can you search here?");
 //        checkoutPage.assertTextWhenReady(searchPage.productShaorma, "SHAORMA LA TURCU");// asta nu merge pentru ca este un bug!
+//        System.out.println("Yes, I see the text3!");
 
     }
+
+    @Test
+    public void searchRaduTheHandsome(){
+        searchPage.searchPage("radu");
+        searchPage.pushSearchButton();
+        searchPage.searchPageClear();
+        checkoutPage.assertTextWhenReady(searchPage.productShaorma, "RADU THE HANDSOME");
+        System.out.println("Yes, I see the text1!");
+
+        searchPage.searchPage("radu the");
+        searchPage.pushSearchButton();
+        searchPage.searchPageClear();
+        checkoutPage.assertTextWhenReady(searchPage.productShaorma, "RADU THE HANDSOME");
+        System.out.println("Yes, I see the text2!");
+
+        searchPage.searchPage("radu the handsome");
+        searchPage.pushSearchButton();
+        searchPage.searchPageClear();
+        checkoutPage.assertTextWhenReady(searchPage.productShaorma, "RADU THE HANDSOME");
+        System.out.println("Yes, I see the text3!");
+    }
+
+    @Test
+    public void searchEngagementRing(){
+        searchPage.searchPage("engagement");
+        searchPage.pushSearchButton();
+        searchPage.searchPageClear();
+        checkoutPage.assertTextWhenReady(searchPage.productShaorma, "ENGAGEMENT RING");
+        System.out.println("Yes, I see the text1!");
+
+        searchPage.searchPage("engagement ring");
+        searchPage.pushSearchButton();
+        searchPage.searchPageClear();
+        checkoutPage.assertTextWhenReady(searchPage.productShaorma, "ENGAGEMENT RING");
+        System.out.println("Yes, I see the text2!");
+
+    }
+
+
 
 
 }
